4869421815b8c79d55969e710f9da066008456b3
Victor Miranda Balduino <66287562+VictorRandom@users.noreply.github.com>
2021-06-20 14:29:27 -0300

    Website-NLW-Final
    Finalizando o site da aula da Next Level Week

---
+++ NLW/src/server.js
@@ -0,0 +1,116 @@
+const express = require("express")
+const server = express()
+
+//pegar o banco de dados
+const db = require("./database/db")
+
+//configurar pasta publica
+server.use(express.static("public"))
+
+//habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({ extended: true }))
+
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
+//configurar caminhos da aplicação
+//pagina inicial
+//req: requisição
+//res:resposta
+server.get("/", (req, res) => {
+    return res.render("index.html", {title: "Um titulo"})
+})
+
+
+
+server.get("/create-point", (req, res) => {
+
+    
+    //req.query: query strings da nossa URL
+    //console.log(req.query)
+
+    return res.render("create-point.html")
+})
+
+server.post("/savepoint", (req, res) => {
+
+    //req.body: o corpo do nosso formulário
+    //console.log(req.body)
+
+    //inserir dados no banco de dados
+        const query = `
+            INSERT INTO places (
+                image,
+                name,
+                address,
+                address2,
+                state,
+                city,
+                items
+            ) VALUES (?,?,?,?,?,?,?);
+        `
+
+        const values = [
+            req.body.image, 
+            req.body.name, 
+            req.body.address, 
+            req.body.address2, 
+            req.body.state, 
+            req.body.city,
+            req.body.items,  
+        ]
+
+        function afterInsertData(err) {
+            if (err)
+            {
+                console.log(err)
+                return res.send("Erro no cadastro!")
+            }
+
+            console.log("Cadastrado com sucesso")
+            console.log(this)
+
+            return res.render("create-point.html", { saved: true })
+        }
+
+        db.run(query, values, afterInsertData)
+
+
+})
+
+
+server.get("/search", (req, res) => {
+
+    const search = req.query.search
+
+    if(search == "") {
+        //pesquisa vazia
+        return res.render("search-results.html", { total: 0 })
+    }
+
+    //pegar os dados do banco de dados
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`,function(err, rows){
+        if(err){
+            return console.log(err)
+        }
+        
+        console.log("Aqui estão seus registros: ")
+        console.log(rows)
+        
+        const total = rows.length
+
+        //mostrar a pagina html com os dados do banco de dados
+        return res.render("search-results.html", {places: rows, total})
+    })
+
+    
+})
+
+
+//ligar o servidor
+server.listen(3000)
